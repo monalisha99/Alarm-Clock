2f770b7f0b8d718ed1ea1bff817a8ded86598452
monalisha99 <bmobalisha1@gmail.com>
2025-08-03 21:52:56 +0530

    creat alarm.py

---
+++ alarm.py
@@ -0,0 +1,48 @@
+
+import tkinter as tk
+from datetime import datetime
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root = tk.Tk()
+
+
+root.mainloop()
+
+
+
+
+
